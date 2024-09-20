832f784b913820a06a0f58d2858ff279c5f0719b
cliz1 <137553415+cliz1@users.noreply.github.com>
2024-09-19 22:36:58 -0400

    Update urls.py

--- cs412/urls.py
+++ cs412/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("quote_of_day/", include("quote_of_day.urls")),
-    path('', include('quote_of_day.urls')),
+    path("quotes/", include("quotes.urls")),
+    path('', include('quotes.urls')),
                                     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
